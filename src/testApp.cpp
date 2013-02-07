cfc433aacc6c5dded25828e275f2ba149818286f
eddycombo <eddy@combosquare.net>
2013-02-07 10:44:12 +0100

    now works

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -7,55 +7,39 @@ cam res 656 x 492
 //--------------------------------------------------------------
 void testApp::setup(){
 
-    //camText.allocate(WIDTH, HEIGHT, GL_LUMINANCE);
-
     ofBackground(0);
     ofSetFrameRate(66);
     bCameraFound = false;
     bCamInit = false;
     bGrabbing = false;
 
-    //camCount = 0;
-
-    //camText.allocate(640, 480, GL_LUMINANCE);
-
-    //camImg.allocate(WIDTH, HEIGHT, OF_IMAGE_GRAYSCALE);
-
     thread.startThread(true, false);
     camImage.allocate(656, 492, OF_IMAGE_GRAYSCALE);
 
     bCamInit = true;
-
-
 }
 
 
 void testApp::update(){
 
-    //thread.lock();
-      //  camImage = thread.mantaImage;
-    //thread.unlock();
-
-
+    thread.lock();
+        camImage = thread.mantaImage;
+    thread.unlock();
 
 
 }
 
 //--------------------------------------------------------------
 void testApp::exit(){
-
     thread.stopThread();
-
-
-
 }
 //--------------------------------------------------------------
 void testApp::draw(){
+    camImage.draw(0, 0);
 
-
-   thread.mantaImage.draw(0, 0);
-
-
+    ofPushMatrix();
+        ofDrawBitmapString(ofToString(ofGetFrameRate()), 10, 10);
+    ofPopMatrix();
 }
 
 //--------------------------------------------------------------
